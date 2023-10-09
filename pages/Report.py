a20fcafc1d7bb194863483c2bfb1d0bcd60eac14
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-10-09 18:16:17 +0530

    Couldn't download portfolio

--- pages/Report.py
+++ pages/Report.py
@@ -2,8 +2,6 @@ import streamlit as st
 from Home import statusFunc
 import streamlit.components.v1 as components
 import imgkit 
-
-
 # with open("output/portfolio.html","r") as f:
 #     report=f.read()
 
@@ -12,14 +10,17 @@ import imgkit
 # st.pyplot()
 status=statusFunc()
 if status == True:
-    img_file=imgkit.from_file('output/portfolio.html','portfolio.jpg')
-    st.image('portfolio.jpg',width=1540)
-    with open("portfolio.jpg", "rb") as file:
-        btn = st.download_button(
-                label="Download Report",
-                data=file,
-                file_name="portfolio.jpg",
-                mime="image/png"
-            )
+    try:
+        img_file=imgkit.from_file('output/portfolio.html','portfolio.jpg')
+        st.image('portfolio.jpg',width=1540)
+        with open("portfolio.jpg", "rb") as file:
+            btn = st.download_button(
+                    label="Download Report",
+                    data=file,
+                    file_name="portfolio.jpg",
+                    mime="image/png"
+                )
+    except:
+        st.info("Couldn't download portfolio")
 
 
